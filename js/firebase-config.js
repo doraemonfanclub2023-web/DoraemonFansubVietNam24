c61cbd21b0440d333075711c7e5fd02871dd19c7
doraemonfanclub2023-web <doraemonfanclub2023@gmail.com>
2026-07-04 22:25:42 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBt3G9n5JYu3EsvqJR9IoW2vRAc_Es3-ws",
   authDomain: "doraemon-fansub-vietnam.firebaseapp.com",
   projectId: "doraemon-fansub-vietnam",
-  // SỬA ĐÚNG DÒNG NÀY THEO CẤU HÌNH CỦA FIREBASE
-  storageBucket: "doraemon-fansub-vietnam.firebasestorage.app", 
+  storageBucket: "doraemon-fansub-vietnam.firebasestorage.app",
   messagingSenderId: "380840935390",
   appId: "1:380840935390:web:60fab4722a9fba5053a74f",
   measurementId: "G-0S2Z4XRK0B"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
